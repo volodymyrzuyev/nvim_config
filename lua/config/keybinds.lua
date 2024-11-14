f563703aa938476e458e807c583d6e3be020de20
Volodymyr <volodymyrzuyev@gmail.com>
2024-11-14 11:01:27 -0600

    buffer close

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -29,5 +29,8 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- bnext bprev
 vim.keymap.set("n", "<Tab>", "<cmd>bn<CR>")
 vim.keymap.set("n", "<leader><Tab>", "<cmd>bp<CR>")
+
+vim.keymap.set("n", "<leader>x", "<cmd>bd<CR>")
