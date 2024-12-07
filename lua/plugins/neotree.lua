6d89db036f2353b451aad1e18eb7937585ba81c7
Volodymyr <volodymyrzuyev@gmail.com>
2024-12-06 20:40:14 -0600

    showing hidden files in neo tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -9,6 +9,13 @@ return {
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
 		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					filtered_items = {
+						visible = true, -- when true, they will just be displayed differently than normal items
+					},
+				},
+			})
 			vim.keymap.set("n", "<leader>E", "<cmd>Neotree last<CR>")
 			vim.keymap.set("n", "<leader>X", "<cmd>Neotree close<CR>")
 		end,
