5ebb8031caa84bca185d26daff450acfde835468
Volodymyr Zuyev <volodymyrzuyev@gmail.com>
2025-02-01 21:17:02 -0600

    clang format, markdown, json

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 require("config.keybinds")
 require("config.options")
-require("config.autocmd")
 require("config.lazy")
+require("config.autocmd")
--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,7 +1,18 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+local jqx = vim.api.nvim_create_augroup("Jqx", {})
+vim.api.nvim_clear_autocmds({ group = jqx })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	pattern = { "*.json", "*.yaml" },
+	desc = "preview json and yaml files on open",
+	group = jqx,
+	callback = function()
+		vim.cmd.JqxList()
+	end,
 })
